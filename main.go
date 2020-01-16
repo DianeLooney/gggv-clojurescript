bb3a5ac74ae72770c5b8953e6d8b8655a6e4d8a7
Diane Looney <diane@dianelooney.com>
2020-01-16 02:45:34 -0500

    Swap to using go watcher

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
+	"time"
+
+	"github.com/radovskyb/watcher"
+)
+
+func spawn(kill chan bool) (done chan bool) {
+	cmd := exec.Command("lumo", "show.cljs")
+
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	stdErr, err := cmd.StderrPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		rd := bufio.NewReader(stdOut)
+		killed := false
+		for {
+			ln, _, err := rd.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				continue
+			}
+			if kill != nil && !killed {
+				kill <- true
+				killed = true
+				<-kill
+			}
+			println(string(ln))
+		}
+	}()
+	go func() {
+		rd := bufio.NewReader(stdErr)
+		for {
+			ln, _, err := rd.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				continue
+			}
+			println(string(ln))
+		}
+	}()
+	done = make(chan bool)
+	go func() {
+		<-done
+		cmd.Process.Kill()
+		done <- true
+		stdOut.Close()
+		stdErr.Close()
+		close(done)
+	}()
+	return
+}
+
+func main() {
+	kill := spawn(nil)
+	w := watcher.New()
+	if err := w.Add("runtime.cljs"); err != nil {
+		panic(err)
+	}
+	if err := w.Add("show.cljs"); err != nil {
+		panic(err)
+	}
+	go w.Start(300 * time.Millisecond)
+	for {
+		select {
+		case e := <-w.Event:
+			fmt.Println(e)
+			kill = spawn(kill)
+		case err := <-w.Error:
+			fmt.Println(err)
+		}
+	}
+}
